2e8b783fdfd494cb28298ea733b26d88fc1f5471
ManuelPH <manuelph96ph@gmail.com>
2022-01-22 18:53:44 +0100

    Evaluación con Singularity

---
+++ output_Eval_singularity.sh
@@ -0,0 +1,32 @@
+A) C++ k-mer13
+Start
+Number of generated k-mers: 67108864
+Finish!
+
+real	0m0,957s
+user	0m0,802s
+sys	0m0,113s
+B) C++ k-mer14
+Start
+Number of generated k-mers: 268435456
+Finish!
+
+real	0m2,987s
+user	0m2,845s
+sys	0m0,110s
+C) Python k-mer13
+Start
+Number of generated k-mers: 67108864
+Finish!
+
+real	1m33,957s
+user	1m33,603s
+sys	0m0,180s
+C) Python k-mer14
+Start
+Number of generated k-mers: 268435456
+Finish!
+
+real	6m21,281s
+user	6m20,372s
+sys	0m0,180s
